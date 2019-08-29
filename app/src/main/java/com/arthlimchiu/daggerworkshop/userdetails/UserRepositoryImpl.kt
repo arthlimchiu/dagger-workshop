166644816045c5bf447314b5bf0f9befcf285572
Arth Limchiu <limchiuarth@gmail.com>
2019-08-29 22:08:38 +0800

    Removed logs

--- app/src/main/java/com/arthlimchiu/daggerworkshop/userdetails/UserRepositoryImpl.kt
+++ app/src/main/java/com/arthlimchiu/daggerworkshop/userdetails/UserRepositoryImpl.kt
@@ -1,6 +1,5 @@
 package com.arthlimchiu.daggerworkshop.userdetails
 
-import android.util.Log
 import com.arthlimchiu.daggerworkshop.Api
 import retrofit2.Call
 import retrofit2.Callback
@@ -8,11 +7,7 @@ import retrofit2.Response
 
 class UserRepositoryImpl(private val api: Api) : UserRepository {
 
-    private var index = 0
-
     override fun getUser(username: String, onSuccess: (user: User) -> Unit, onFailure: (t: Throwable) -> Unit) {
-        index++
-        Log.d("UserRepository", "Index: $index")
         api.getUser(username).enqueue(object : Callback<User> {
             override fun onResponse(call: Call<User>, response: Response<User>) {
                 response.body()?.let { user ->
